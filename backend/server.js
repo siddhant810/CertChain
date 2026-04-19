f8999c0c80e7591ab656b5baae1abd1094e6f344
siddhant810 <siddhant8102004@gmail.com>
2026-04-19 16:53:51 +0530

    Fixed port for render deployment

--- backend/server.js
+++ backend/server.js
@@ -171,10 +171,6 @@ app.post("/revoke-certificate", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("✅ Server running on http://localhost:5000");
-});
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
