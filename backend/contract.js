2d54acae8558ec5c5307c05edb51ac3c9749e490
siddhant810 <siddhant8102004@gmail.com>
2026-04-19 17:46:08 +0530

    hardcode fix

--- backend/contract.js
+++ backend/contract.js
@@ -1,14 +1,17 @@
 const { ethers } = require("ethers");
-require("dotenv").config();
 
-// ✅ Provider (Sepolia via Alchemy)
-const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_URL);
+// ⚠️ TEMP FIX (for debugging only)
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://eth-sepolia.g.alchemy.com/v2/0guzWarJLHawQ0TQxzp_Q"
+);
 
-// ✅ Wallet (from ENV)
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const wallet = new ethers.Wallet(
+  "317f66e064447c1ffbb70e432c50c3808f6f5a6e6e08a79cdb1aaae2eaa8e3c8", // without 0x or with 0x both fine
+  provider
+);
 
-// ✅ Your deployed contract
 const contractAddress = "0xfCB8F412D676BBD5Ac84Abb1be0138b2Db95d961";
+
 
 // ABI must exactly match CertificateVerification.sol
 const contractABI = [
